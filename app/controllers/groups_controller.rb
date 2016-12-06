ab2a8d3844be830f18b700c812c8292ab9be0554
岩井　智夢 <tomo42695@gmail.com>
2016-12-06 18:29:32 +0900

    edit groups first

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_groups, only: [:update, :edit]
   def index
     @group = Group.find(1)
   end
@@ -10,14 +11,23 @@ class GroupsController < ApplicationController
   def edit
   end
 
+  def update
+    @group.update(groups_params)
+    redirect_to action: :index
+  end
+
   def create
     @group = Group.create(groups_params)
-    redirect_to action: :index, controller: :messages
+    redirect_to action: :index
   end
 
   private
 
   def groups_params
     params.require(:group).permit(:name, {user_ids: [] })
+  end
+
+  def set_groups
+    @group = Group.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :groups, only: [:index, :new, :edit, :create]
+  resources :groups, only: [:index, :new, :edit, :create, :update]
 end
